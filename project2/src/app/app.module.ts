514302d72c6159358102634c85b3c96922e44aae
a_long <adriana.michelle.long@gmail.com>
2019-11-02 22:38:46 -0400

    pulling

--- project2/src/app/app.module.ts
+++ project2/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule, routnComps } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ProfileComponent} from './components/profile/profile.component';
@@ -8,6 +8,7 @@ import { MainFeedComponent } from './components/main-feed/main-feed.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { GoHomeComponent } from './components/go-home/go-home.component';
 import { CreatePostComponent } from './components/create-post/create-post.component';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -23,6 +24,7 @@ import { CreatePostComponent } from './components/create-post/create-post.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
--- project2/src/app/components/login/login.component.ts
+++ project2/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { NgForm } from "@angular/forms";
+import { format } from  "url";
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {
+  ngOnInit()  {}
+
+
+  submission(form: NgForm){
+    this.http
+    .post("http://localhost:8080/Spring-MVC/auth.app", {
+    //////last section of this matches the name of the controller
+      name: form,
+      age: form.value.age
+      ///change this to username and password later
+    })
+    .toPromise()
+    .then((r: {name: string; age: number}) => localStorage.setItem("username", r.name)
+    );
+  }
+
+
+  authorize() {
+    this.http
+    .post("http://localhost:8080/Spring-MVC/authorize.app", {
+
+
+    name: localStorage.getItem("username"),
+    age: 0 
+    })
+
+    .toPromise()
+    .then(r => console.log(r));
+
+
   }
 
 }
