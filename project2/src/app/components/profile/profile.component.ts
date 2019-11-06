4cbd10f2c30230f2537dd63c8aabcba561b7989d
brianabajah <abajahbrian@gmail.com>
2019-11-05 19:15:52 -0500

    Profile

--- project2/src/app/components/profile/profile.component.ts
+++ project2/src/app/components/profile/profile.component.ts
@@ -18,11 +18,15 @@ export class ProfileComponent implements OnInit {
     this.profpic=localStorage.getItem("profpic");
     this.description=localStorage.getItem("description");
       if(this.profpic==null || this.profpic.length<1){
+        console.log("pic area");
       this.profpic="../../../assets/img/alt.png";
       }
       if(this.description==null || this.description.length<1){
         this.description="Description"
       }
+  }
+  getprof(){
+    // this.prof();
   }
   
 }
