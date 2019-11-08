dddb693eefdfce28f5a000f0e473c4bc47e1603b
a_long <adriana.michelle.long@gmail.com>
2019-11-08 07:58:52 -0500

    I don't see names

--- project2/src/app/app.component.ts
+++ project2/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +7,9 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   constructor(private router: Router ) {
-
   }
   title = 'project2';
   menubtn:string="/"
-
   swic():void{
     console.log(this.router.url);
     if(this.router.url == '/' || this.router.url == '/reg' ){
@@ -25,7 +22,22 @@ export class AppComponent {
     this.swic();
     this.router.navigate([{outlets: {primary: 'main-feed' ,mleft: 'profile'}}]);
   }
-
+  logout(){
+    localStorage.clear();
+    this.router.navigateByUrl("/");
+    console.log("logout");
+  }
+  where(){
+    return (this.router.url === '/' || this.router.url === '/reg');
+  }
+  search(){
+  // @ts-ignore
+  let j = document.getElementById("look").value;
+  console.log(j);
+    let addrs= '/profile/'+j;
+    this.router.navigateByUrl(addrs);
+  }
 }
+
 
 
